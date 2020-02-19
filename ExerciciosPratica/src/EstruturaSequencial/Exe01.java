9ade1a067cbe35fb16c7d86fd3b52ebc93f89653
Douglas Cardoso <douglascf12@live.com>
2020-02-19 11:30:04 -0300

    classe Exe01 criada e exercicio 1 feito.

---
+++ ExerciciosPratica/src/EstruturaSequencial/Exe01.java
@@ -0,0 +1,11 @@
+package EstruturaSequencial;
+
+public class Exe01 {
+	
+	//01. Faça um programa que mostre a mensagem "Olá Mundo!" na tela.
+
+	public static void main(String[] args) {
+		System.out.println("Olá Mundo!");
+	}
+	
+}
